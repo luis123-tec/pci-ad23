dd672be347888caada8a3b366114451891d1257f
luis123-tec <141784964+luis123-tec@users.noreply.github.com>
2023-09-10 11:55:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
 from hangman_words import word_list
 from hangman_art import logo, stages
+#Import all the necesarry modules for the game to start
 
 def start_game():
-#Definir funciones para el inicio del juego (Sacar las palabras, determinar estado, vidas y score)
+    #Define the functions for the start of the game (word, len, lives, display)
     chosen_word = random.choice(word_list)
     word_length = len(chosen_word)
 
@@ -18,39 +19,53 @@ def start_game():
     return chosen_word, word_length, end_of_game, lives, max_score, display
 
 def play_hangman():
-  #Mandar llamar la funcion para que se ejecute el incio del juego
+    #Call in the function and the variables to start the game
     chosen_word, word_length, end_of_game, lives, max_score, display = start_game()
 
-    while not end_of_game:
-        guess = input("Guess a letter: ").lower()
+    while True:
+        while not end_of_game:
+            #Game logic
+            guess = input("Guess a letter: ").lower()
 
-        if guess in display:
-            print(f"You've already guessed {guess}")
+            if guess in display:
+                print(f"You've already guessed {guess}")
 
-        for position in range(word_length):
-            letter = chosen_word[position]
+            for position in range(word_length):
+                letter = chosen_word[position]
 
-            if letter == guess:
-                display[position] = letter
+                if letter == guess:
+                    display[position] = letter
 
-        if guess not in chosen_word:
-            print(f"You guessed {guess}, that's not in the word. You lose a life.")
-            lives -= 1
-            if lives == 0:
+            if guess not in chosen_word:
+                print(f"You guessed {guess}, that's not in the word. You lose a life.")
+                lives -= 1
+                if lives == 0:
+                    end_of_game = True
+                    print("You lose.")
+
+            print(" ".join(display))
+
+            if "_" not in display:
                 end_of_game = True
-                print("You lose.")
+                print("You win.")
+                score = max_score + lives * 100  # Incrementa el score
+                print(f"Your score is {score}")
 
-        print(" ".join(display))
+            print(stages[lives])
 
-        if "_" not in display:
-            end_of_game = True
-            print("You win.")
-            score = max_score + lives * 100  # Incrementa el score
-            print(f"Your score is {score}")
+        restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+      #Input to see if you want to play again or not
 
-        print(stages[lives])
+        if restart == "no":
+            print("Goodbye")
+            break
+        elif restart == "yes":
+            print("Let's play again!")
+            chosen_word, word_length, end_of_game, lives, max_score, display = start_game()
+        else:
+            print("Invalid input. Please type 'yes' or 'no'.")
 
 if __name__ == "__main__":
     play_hangman()
 
-#Como hay tres modulos, cuando se este en main se va a ejectuar el juego
+#Since there are 3 modules, by being in main the game starts
